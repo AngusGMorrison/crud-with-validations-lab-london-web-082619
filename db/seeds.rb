b11ba1e64e2ea015798501b9cd137af50b600ca2
Angus Morrison <contact@angus-morrison.com>
2019-09-22 19:30:24 +0100

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+Song.create(
+  title: "Song 1",
+  released: true,
+  release_year: 1999,
+  artist_name: "Artist 1",
+  genre: "Pop"
+)
+
+Song.create(
+  title: "Song 2",
+  released: true,
+  release_year: 1999,
+  artist_name: "Artist 2",
+  genre: "Pop"
+)
